134793fb486c9fb13253a6659874c6cbc9e664fb
kca2250 <2250katok@gmail.com>
2025-06-08 23:58:21 +0900

    first commit

---
+++ wezterm.lua
@@ -0,0 +1,77 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.automatically_reload_config = true
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+config.font_size = 17.0
+config.use_ime = true
+config.window_background_opacity = 0.85
+config.macos_window_background_blur = 20
+
+config.initial_cols = 135
+config.initial_rows = 45
+
+-- タイトルバーを非表示
+config.window_decorations = "RESIZE"
+-- タブバーの表示
+-- config.show_tabs_in_tab_bar = true
+-- タブが一つの時は非表示
+-- onfig.hide_tab_bar_if_only_one_tab = true
+-- falseにするとタブバーの透過が効かなくなる
+-- config.use_fancy_tab_bar = true
+
+-- タブバーの透過
+config.window_frame = {
+  font_size = 16.0,
+  inactive_titlebar_bg = "none",
+  active_titlebar_bg = "none",
+}
+
+-- windowのサイズ
+config.window_padding = {
+  left = 4,
+  right = 4,
+  top = 9,
+  bottom = 0,
+}
+
+-- タブバーを背景色に合わせる
+config.window_background_gradient = {
+  colors = { "#000000" },
+}
+
+-- タブの追加ボタンを非表示
+config.show_new_tab_button_in_tab_bar = false
+
+-- タブ同士の境界線を非表示
+config.colors = {
+  cursor_fg = "#FFFFFF",
+  cursor_bg = "#ff5733",
+  tab_bar = {
+    inactive_tab_edge = "none",
+  },
+}
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local background = "#5d6d7e"
+  local foreground = "#FFFFFF"
+  local edge_background = "none"
+  if tab.is_active then
+    background = "#ff5733"
+    foreground = "#FFFFFF"
+  end
+  local edge_foreground = background
+  local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+  return {
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+    { Background = { Color = background } },
+    { Foreground = { Color = foreground } },
+    { Text = title },
+    { Background = { Color = edge_background } },
+    { Foreground = { Color = edge_foreground } },
+  }
+end)
+
+return config
+
